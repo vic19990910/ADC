b6ed228514ce74bf478592e54734d7dc57cd53cb
vic19990910 <80896893+vic19990910@users.noreply.github.com>
2021-03-18 21:54:28 +0800

    Add files via upload

--- MQ3/MQ3.ino
+++ MQ3/MQ3.ino
@@ -1,1 +1,23 @@
+//
+// MQ3: Alcohol Sensor
+//
+//int TIME_UNTIL_WARMUP = 900;
+//unsigned long time;
+  int alco;
+void setup() {
+  Serial.begin(115200);
+  pinMode(A0, INPUT);
 
+  // warm-up MQ-3 for 15 minutes
+  //time = millis()/1000;
+  //while(time<=TIME_UNTIL_WARMUP);
+  
+  // warm-up MQ-3 till reading value <600
+  while (analogRead(A0)>600);
+}
+
+void loop() {
+  alco = analogRead(A0);
+  Serial.println(alco);
+  delay(50);
+}
