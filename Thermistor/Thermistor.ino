be03a73a1f8ca2af5c5fcfbbe4894d3ca837ff13
vic19990910 <80896893+vic19990910@users.noreply.github.com>
2021-03-18 21:30:22 +0800

    Add files via upload

--- Thermistor/Thermistor.ino
+++ Thermistor/Thermistor.ino
@@ -1,1 +1,55 @@
+/*
+  Analog Input
 
+  Demonstrates analog input by reading an analog sensor on analog pin 0 and
+  turning on and off a light emitting diode(LED) connected to digital pin 13.
+  The amount of time the LED will be on and off depends on the value obtained
+  by analogRead().
+
+  The circuit:
+  - potentiometer
+    center pin of the potentiometer to the analog input 0
+    one side pin (either one) to ground
+    the other side pin to +5V
+  - LED
+    anode (long leg) attached to digital output 13
+    cathode (short leg) attached to ground
+
+  - Note: because most Arduinos have a built-in LED attached to pin 13 on the
+    board, the LED is optional.
+
+  created by David Cuartielles
+  modified 30 Aug 2011
+  By Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/AnalogInput
+*/
+#include <math.h>
+int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorValue = 0;  // variable to store the value coming from the sensor
+  double T, Temp;
+  double K=301.15; 
+  double lnR;
+  int16_t R;// Thermistor resistence
+  int16_t L = 8805;  // calibrated by reading R at room temperature (=28 degree Celsius )
+  int16_t B  = 3950;
+  int16_t Pullup = 10000; // 10K ohm
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+    // R / (Pullup + R)   = adc / 4096
+  R = (Pullup * sensorValue) / (4096 -sensorValue);
+  // B = (log(R) -log(R0)) / (1/T -1/T0) 
+  T = 1 / (1/K + (log(R)-log(L)) / B );
+  Temp = T -273.15;
+  Serial.println(Temp);
+  delay(500);
+}
